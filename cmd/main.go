e45e6f5cd78f6c96902f32f1fb83dafd1c591e25
RArdent <jahongirswe@gmail.com>
2022-07-04 16:43:50 +0500

    Make initial fiber connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
+func main() {
+	r := fiber.New()
+
+	r.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Test")
+	})
+
+	r.Listen(":8000")
 }
