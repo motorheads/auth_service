43db042798cff9a49745415506988252741a28fa
RArdent <jahongirswe@gmail.com>
2022-07-04 18:17:28 +0500

    Update routes

--- api/api.go
+++ api/api.go
@@ -1,1 +1,27 @@
 package api
+
+import "github.com/gofiber/fiber/v2"
+
+func Register(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"status": "ok",
+	})
+}
+
+func Login(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"status": "ok",
+	})
+}
+
+func User(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"status": "ok",
+	})
+}
+
+func Logout(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"status": "ok",
+	})
+}
--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,19 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/motorheads/auth_service/api"
+)
 
 func New() *fiber.App {
-	r := fiber.New()
+	router := fiber.New()
 
-	r.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Test")
-	})
+	router.Group("/api")
 
-	return r
+	router.Post("/api/register", api.Register)
+	router.Post("/api/login", api.Login)
+	router.Get("/api/user", api.User)
+	router.Post("/api/logout", api.Logout)
+
+	return router
 }
